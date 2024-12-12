4678599c530e9d0505f461b90eaabba971bdba17
wxss <2198993328@qq.com>
2024-12-12 11:35:05 +0800

    wxss: Add Shell mcDTRU-X1 On Thu Dec 12 11:33:42 2024

---
+++ Shell/mcDTRU230-X1.sh
@@ -0,0 +1,75 @@
+### 上下行频率等配置 ###
+show_status
+### 防自动重启 ###
+cli write_fpgareg 0x7d 0
+### 时钟锁 ###
+alm
+### 载波分级配置文件 ###
+cd /mnt/flash/metadata/z/pdt
+### 软件版本 ###
+v
+### 配置 FPGA ###
+cd /tmp
+# tftp 下载配置包
+tftp 192.168.114.23 -gr app_start.sh
+# tftp 上传配置包
+tftp 192.168.114.23 -pr app_start.sh
+upl app_start.sh
+# 删除旧配置
+rm -rf /mnt/flash/*
+# 解压
+tar -xvJf zg60_240914.txz -C /
+
+
+****起振点****
+cli
+admin
+lmk_inchannelconfig 0
+set_rx1freq 255000000
+set_rx2freq 257000000
+set_rx3freq 255000000
+set_rx4freq 257000000
+set_tx1freq 310000000
+
+
+****RX链路****
+cli
+admin
+lmk_inchannelconfig 0
+# RX on
+write_fpgareg 0x259 2
+# TX on
+write_fpgareg 0x259 1
+
+set_multi_carr_mode 3 2
+freq_config 0 230
+freq_config 1 230
+freq_config 2 230
+
+powercalc 10 10000000
+powercalc 11 10000000
+powercalc 12 10000000
+powercalc 13 10000000
+powercalc 18 10000000
+powercalc 19 10000000
+
+
+****TX链路****
+lmk_inchannelconfig 0
+set_testsource
+dl_tetrasource 5120 0 0
+
+set_multi_carr_mode 1 1
+freq_config 0 231
+set_pa_input_sig_sw 1
+write_fpgareg 0x46 1
+write_txdvga 2
+powercalc 0 1000000
+write_fbdvga 7
+powercalctxfb 14 1000000
+
+
+cd mnt/flash/patch/bin/
+mv dtru_fpga.gew.bin dtru_fpga.gew.bbk
+dow dtru_fpga.gew2.bin
+mv dtru_fpga.gew2.bin dtru_fpga.gew.bin
